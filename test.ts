c18a0d430f2e4b0529967f5627e57849fb2d7a60
Khushraj Rathod <khushraj.rathod@gmail.com>
2021-03-01 21:15:43 +0530

    Don't sanitise ops and resources

--- test.ts
+++ test.ts
@@ -8,23 +8,28 @@ listenAndServe({ port }, (req) => {
     req.respond({ status: 200, body: exchange })
 })
 
-Deno.test("Self HTTP Request via ngrok", async () => {
-    let ngrokInstance: Ngrok
+Deno.test({
+    name: "Self HTTP Request via ngrok",
+    fn: async () => {
+        let ngrokInstance: Ngrok
 
-    const ip = await new Promise(resolve => {
-        Ngrok.create({
-            protocol: "http",
-            port,
-        }).then(ngrok => {
-            ngrokInstance = ngrok
-            ngrok.addEventListener("ready", (event) => {
-                resolve(event.detail)
+        const ip = await new Promise(resolve => {
+            Ngrok.create({
+                protocol: "http",
+                port,
+            }).then(ngrok => {
+                ngrokInstance = ngrok
+                ngrok.addEventListener("ready", (event) => {
+                    resolve(event.detail)
+                }, { once: true })
             })
         })
-    })
 
-    const response = await fetch(`https://${ip}`).then(r => r.text())
-    assertEquals(response, exchange)
+        const response = await fetch(`https://${ip}`).then(r => r.text())
+        assertEquals(response, exchange)
 
-    ngrokInstance!.destroy()
+        ngrokInstance!.destroy()
+    },
+    sanitizeOps: false,
+    sanitizeResources: false
 })
